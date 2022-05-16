d697280fd5b4b5753ed398fb2a3395b6b4d4cdf8
naokoski <naoko.skiada@kcl.ac.uk>
2022-05-16 16:23:21 +0100

    initial package documentation

--- R/models.R
+++ R/models.R
@@ -2,12 +2,31 @@ library(tidyverse)
 library(poLCA)
 library(foreach)
 
+# Estimate LC models with different number of classes
+
+
 #' this function takes the maximum number of classes one wants to run
 #' then runs models from 1 class to the maximum amount of classes
 #' outputs the result as a list
 #' user needs to define the model, maximum iterations and number of repetitions
 
-#### Function to run all class models in parallel if needed (has not been possible)
+
+#' Estimate required LC models with different number of classes
+#'
+#' @param data The dataset used
+#' @param k The maximum number of classes required
+#' @param formula The LC model (as documented in poLCA)
+#' @param maxiter The maximum number of iterations
+#' @param nrep Number of times that model should be estimated using different starting values
+#'
+#' @return A list
+#' @export
+#'
+#' @examples
+#' Estimate LC models with 1,2,3 and 4 classes
+#' Each model to iterate up to 5,000 times and
+#' repeated 10 times.
+#' models(data, 4, f, 5000, 10)
 
 models <- function(data, k, formula, maxiter, nrep) {
   foreach(i = 1:k, .packages = "poLCA") %do%
