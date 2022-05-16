a3978239db6361f06a015670218b73ad79afc38f
naokoski <naoko.skiada@kcl.ac.uk>
2022-05-16 14:37:39 +0100

    removed formula function call.

--- R/models.R
+++ R/models.R
@@ -7,14 +7,6 @@ library(foreach)
 #' outputs the result as a list
 #' user needs to define the model, maximum iterations and number of repetitions
 
-#### Define LCA regression model. Complete case analysis (270 cases excl.)
-
-f <- cbind(II, III, IV, VI,VII,
-           IX, X, XI, XII, XIII, XIV) ~ Age2018_categories + Gender + Ethnicity_collapsed +
-  F2 + F30 + F31 + IMD15_Quintile
-
-
-
 #### Function to run all class models in parallel if needed (has not been possible)
 
 models <- function(data, k, formula, maxiter, nrep) {
